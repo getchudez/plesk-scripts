aa96bafd84fe43962f2c07ff4081b32044eb7832
Gustavo Etchudez <getchudez@gmail.com>
2014-07-11 10:10:56 -0400

    Added find variable and option to find only directory and delete this

--- clean-old-mysql-bkp.sh
+++ clean-old-mysql-bkp.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #
-# Version: V1.0 - 07-10-2014
+# Version: V1.0.1 - 07-10-2014
 # Author: Gustavo Etchudez - mail: getchudez@yandex.com
 # URL: https://github.com/getchudez
 #
 # This script will clean old backup of MySQL created by bkp-mysql.sh
 #
 #
-BACKUP_DIR="/data/backup/mysql"
+BACKUP_DIR="/data/backups/mysql"
 RETDAY="6"
+FIND_BIN=$(which find)
 
-find $BACKUP_DIR -mtime +$RETDAY -exec rm -f {} \;
+$FIND_BIN $BACKUP_DIR -type d -mtime +$RETDAY -exec rm -f {} \;
